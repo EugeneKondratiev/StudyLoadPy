7a30e96b0d94bb6c96a0c333162f17a1722b7679
GreamReaper <GreamReaper@GREAMREAPER->
2020-05-10 23:44:36 +0300

    бляяяяяя

--- src/main.py
+++ src/main.py
@@ -4,4 +4,6 @@ wb = load_workbook("resources\\ІТтаКБ. Сем I. Форма навчанн
 ws = wb["Робота кафедри"]
 wcell1 = ws.cell(20, 2)
 wcell1.value = "PYTHON HERNYA"
+wcell2 = ws.cell(21, 2)
+wcell2.value = "Пайтон Херня"
 wb.save("resources\\ІТтаКБ. Сем I. Форма навчання  денна.xlsx")
