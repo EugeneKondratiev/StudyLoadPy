ba7622bf36df4299fe61d4a3b369b3ee39736de2
EugeneKondratiev <albrus10@mail.ru>
2020-05-26 01:43:43 +0300

    my work is done for now

--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
 import sys
 from PyQt5 import QtWidgets, QtGui
-
-from src.DennaSem1 import DennaSem1
-from src.DennaSem2 import DennaSem2
 from src.QtGUI import AppGUI
-from src.ZaochnaSem2 import ZaoсhnaSem2
-from src.ZaoсhnaSem1 import ZaoсhnaSem1
 
-#p1 = DennaSem1()
-#p2 = ZaoсhnaSem1()
-#p4 = DennaSem2()
-#p3 = ZaoсhnaSem2()
-#p5 = pars()
 if __name__ == "__main__":
     QtWidgets.QApplication.setStyle('Fusion')
     app = QtWidgets.QApplication(sys.argv)
