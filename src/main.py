de7440415e77e90f0ec9f3af1b96c4edb2aa6ee7
GreamReaper <GreamReaper@GREAMREAPER->
2020-05-31 22:35:32 +0300

    edit

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ import sys
 from PyQt5 import QtWidgets, QtGui
 from src.QtGUI import AppGUI
 
-QtWidgets.QApplication.setStyle('Fusion')
-app = QtWidgets.QApplication(sys.argv)
-app.setWindowIcon(QtGui.QIcon('resources\\логотип_коротко_уменьшено.png'))
-window = AppGUI()
-window.show()
-app.exec_()
-
+if __name__ == "__main__":
+    QtWidgets.QApplication.setStyle('Fusion')
+    app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon('resources\\логотип_коротко_уменьшено.png'))
+    window = AppGUI()
+    window.show()
+    app.exec_()
 
