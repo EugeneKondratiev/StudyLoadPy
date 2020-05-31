cbd5c5172163ee8f3b6eab58a23b2eb8bb2fb1a5
EugeneKondratiev <albrus10@mail.ru>
2020-05-31 16:16:49 +0300

    done

--- src/QtGUI.py
+++ src/QtGUI.py
@@ -4,6 +4,7 @@ from src.Doc1 import doc1
 from src.convert import ConverterXLS
 from src.design import Ui_MainWindow
 from src.pars import pars
+from openpyxl import *
 
 
 class AppGUI(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -20,112 +21,80 @@ class AppGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         self.default_file_names = []
         self.default_file_names_rnp = []
         self.radioButton.click()
-        self.subjects = ['Дискретна математика',
-                         'Додатково годин на',
-                         'Години на ЕК',
-                         'Дипломне проектування',
-                         'Теорія інформації та кодування',
-                         'Математична логіка',
-                         'Переддипломна практика',
-                         'Науково-дослідна практика',
-                         'Структурне та логічне програмування',
-                         'Технології комп\'ютерного проектування',
-                         'Методи аналізу даних та їх організація',
-                         'Методи та засоби комп\'ютерних iнформацiйних технологiй',
-                         'Інформаційні технології управління виробництвом ',
-                         'Технології розподілених систем та паралельних обчислень',
-                         'Грід-системи та технології  хмарних обчислень',
-                         'Комп\'ютерний та технічний дизайн',
-                         'Комп\'ютерна графіка',
-                         'Проектування в середовищі машинобудівних CAD',
-                         'Геометричне моделювання та методи візуалізації',
-                         'Алгоритмізація та програмування',
-                         'Програмування',
-                         'Алгоритми та методи обчислень та чисельні методи',
-                         'Об\'єктно-орiєнтоване програмування',
-                         'Проектно-технологічна практика',
-                         'Алгоритми та методи обчислень',
-                         'Методи та системи підтримки прийняття рішень',
-                         'Розробка Web-систем',
-                         'Моделювання систем',
-                         'Чисельні методи',
-                         'Методологія наукових досліджень',
-                         'Веб технології та веб дизайн',
-                         'Інформаційно-аналітична діяльність у галузі інформаційної безпеки',
-                         'Інтелектуальний аналіз даних',
-                         'Органiзацiя баз даних та знань',
-                         'Методи та системи штучного iнтелекту',
-                         'Зосередженi та розподiленi системи управлiння базами даних',
-                         'Операційні системи',
-                         'Управління IT проектами',
-                         'Інформаційні технології',
-                         'Експертні системи',
-                         'Розробка програмного забезпечення',
-                         'Інформатика і системологія',
-                         'Основи автоматизованого проектування складних об`єктiв i систем',
-                         'Інформатика та програмування',
-                         'Крос-платформне програмування',
-                         'Паралельне та багатопоточне програмування',
-                         'Процедурне програмування',
-                         'Сучасні системи управління базами даних ',
-                         'Системний аналiз та проектування компютерних інформаційних систем',
-                         'Моделі технології пректування та управління інформаційними системами',
-                         'Чинники успішного працевлаштування',
-                         'Технології захисту інформації',
-                         'Теорія алгоритмів',
-                         'Математичні методи дослідження операцій',
-                         'Моделювання процесів та об\'єктів енергетичного машинобудування']
-        print(len(self.subjects))
-        self.lectors = ['Плотніков В. М.',
-                        'Становська Т. П.',
-                        'Мазурок І. Є.',
-                        'Мазурок Т. Л.',
-                        'Ломовцев П. Б.',
-                        'Антонова А. Б.',
-                        'Селіванова А. В.',
-                        'Ольшевская',
-                        'Котлик С. В.',
-                        'Корнієнко Ю. К.',
-                        'Тройнина',
-                        'Сіромля С. Г.',
-                        'Швець Н. В.',
-                        'Попков Д. М.',
-                        'Владімірова В. Б.',
-                        'Бодюл О. С.']
+        self.lectors = []
+        self.assisstents = []
+        self.subjects = []
+        self.load_db()
 
-        self.lectors.sort()
-        self.subjects.sort()
-        self.assisstents = ['Плотніков В. М.',
-                        'Становська Т. П.',
-                        'Мазурок І. Є.',
-                        'Мазурок Т. Л.',
-                        'Ломовцев П. Б.',
-                        'Антонова А. Б.',
-                        'Селіванова А. В.',
-                        'Ольшевская',
-                        'Котлик С. В.',
-                        'Корнієнко Ю. К.',
-                        'Тройнина',
-                        'Сіромля С. Г.',
-                        'Швець Н. В.',
-                        'Попков Д. М.',
-                        'Владімірова В. Б.',
-                        'Бодюл О. С.']
-        self.assisstents.append('Болтач С.В.')
-        self.assisstents.append('Асланов')
-        self.assisstents.append('Снігур Т.С.')
-        self.assisstents.append('Бойцова')
-        self.assisstents.append('Кононович')
-        self.assisstents.sort()
-
-        self.comboBox_Assisstents.addItems(self.assisstents)
-        self.comboBox_Lectors.addItems(self.lectors)
-        self.comboBox_Subjects.addItems(self.subjects)
         self.pushButton_plus_asistent.clicked.connect(self.add_assisstant)
         self.pushButton_plus_lector.clicked.connect(self.add_lector)
         self.pushButton_minus_assisstent.clicked.connect(self.minus_assisstant)
         self.pushButton_minus_lector.clicked.connect(self.minus_lector)
+        self.pushButton_plus_asistent_db.clicked.connect(self.add_assisstant_db)
+        self.pushButton_plus_lector_db.clicked.connect(self.add_lector_db)
+        self.pushButton_plus_predmet_db.clicked.connect(self.add_subject_db)
+        self.pushButton_minus_assisstent_db.clicked.connect(self.remove_assisstant_db)
+        self.pushButton_minus_lector_db.clicked.connect(self.remove_lector_db)
+        self.pushButton_minus_predmet_db.clicked.connect(self.remove_subject_db)
+        self.pushButton_Save_db.clicked.connect(self.save_db)
         self.pushButton_Calculate_load.clicked.connect(self.calculate_load)
+
+    def save_db(self):
+        workBookDB = load_workbook("resources\\DB_study_load.xlsx")
+        sheet = workBookDB["Sheet1"]
+        for i in range(2, sheet.max_row):
+            w_cell = sheet.cell(row=i, column=1)
+            w_cell.value = ""
+        for i in range(2, sheet.max_row):
+            w_cell = sheet.cell(row=i, column=2)
+            w_cell.value = ""
+        for i in range(2, sheet.max_row+10):
+            w_cell = sheet.cell(row=i, column=3)
+            w_cell.value = ""
+        for i in range(0, self.listWidget_subjects_db.count()):
+            w_cell = sheet.cell(row=i+2, column=3)
+            w_cell.value = self.listWidget_subjects_db.item(i).text()
+        for i in range(0, self.listWidget_lectors_db.count()):
+            w_cell = sheet.cell(row=i+2, column=1)
+            w_cell.value = self.listWidget_lectors_db.item(i).text()
+        for i in range(0, self.listWidget_assisstents_db.count()):
+            w_cell = sheet.cell(row=i+2, column=2)
+            w_cell.value = self.listWidget_assisstents_db.item(i).text()
+        workBookDB.save("resources\\DB_study_load.xlsx")
+        workBookDB.close()
+        self.load_db()
+
+    def load_db(self):
+        self.lectors = []
+        self.assisstents = []
+        self.subjects = []
+        self.listWidget_subjects_db.clear()
+        self.listWidget_assisstents_db.clear()
+        self.listWidget_lectors_db.clear()
+        self.comboBox_Subjects.clear()
+        self.comboBox_Lectors.clear()
+        self.comboBox_Assisstents.clear()
+        workBookDB = load_workbook("resources\\DB_study_load.xlsx")
+        sheet = workBookDB.active
+        for i in range(2, sheet.max_row):
+            if sheet.cell(column=1, row=i).value is not None:
+                self.lectors.append(sheet.cell(column=1, row=i).value)
+        for i in range(2, sheet.max_row):
+            if sheet.cell(column=2, row=i).value is not None:
+                self.assisstents.append(sheet.cell(column=2, row=i).value)
+        for i in range(2, sheet.max_row):
+            if sheet.cell(column=3, row=i).value is not None:
+                self.subjects.append(sheet.cell(column=3, row=i).value)
+        self.subjects.sort()
+        self.assisstents.sort()
+        self.lectors.sort()
+        self.listWidget_assisstents_db.addItems(self.assisstents)
+        self.listWidget_lectors_db.addItems(self.lectors)
+        self.listWidget_subjects_db.addItems(self.subjects)
+        self.comboBox_Assisstents.addItems(self.assisstents)
+        self.comboBox_Lectors.addItems(self.lectors)
+        self.comboBox_Subjects.addItems(self.subjects)
+        workBookDB.close()
 
     def calculate_load(self):
         array_lectors = []
@@ -137,6 +106,27 @@ class AppGUI(QtWidgets.QMainWindow, Ui_MainWindow):
         print(self.comboBox_Subjects.currentText())
         print(array_lectors)
         print(array_assisstents)
+
+    def add_assisstant_db(self):
+        self.listWidget_assisstents_db.addItem(self.lineEdit_Assisstent.text())
+        self.lineEdit_Assisstent.clear()
+
+    def add_lector_db(self):
+        self.listWidget_lectors_db.addItem(self.lineEdit_Lector.text())
+        self.lineEdit_Lector.clear()
+
+    def add_subject_db(self):
+        self.listWidget_subjects_db.addItem(self.lineEdit_predmet.text())
+        self.lineEdit_predmet.clear()
+
+    def remove_assisstant_db(self):
+        self.listWidget_assisstents_db.takeItem(self.listWidget_assisstents_db.currentRow())
+
+    def remove_lector_db(self):
+        self.listWidget_lectors_db.takeItem(self.listWidget_lectors_db.currentRow())
+
+    def remove_subject_db(self):
+        self.listWidget_subjects_db.takeItem(self.listWidget_subjects_db.currentRow())
 
     def add_assisstant(self):
         self.listWidget_assisstents.addItem(self.comboBox_Assisstents.currentText())
